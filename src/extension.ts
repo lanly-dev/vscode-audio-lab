4cf71b5031747389a076795563dd476188672718
Lan Ly <lanly-dev@users.noreply.github.com>
2026-08-18 16:11:19 -0700

    ref: organize commands

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,19 @@ export async function activate(context: ExtensionContext) {
   const rc = commands.registerCommand
 
   const p = await LemonadeTreeDataProvider.createOrGet()
-  const d1a = rc('audio-lab.transcribeAudioFile', () => transcribeAudio(p))
-  const d1b = rc('audio-lab.transcribeAudioItem', (item: TreeItem) => transcribeAudio(p, item.tooltip?.toString()))
+  const d1 = rc('audio-lab.internal.pickModel', async (modelId: string) => pickModel(modelId, p))
 
-  const d2 = rc('audio-lab.internal.pickModel', async (modelId: string) => pickModel(modelId, p))
+  // No command palette
+  const d2 = rc('audio-lab.ncp.transcribeAudioItem', (item: TreeItem) => transcribeAudio(p, item.tooltip?.toString()))
+  const d3 = rc('audio-lab.ncp.revealInExplorer', revealInExplorer)
 
-  const d3 = rc('audio-lab.changeServerUrl', () => changeServerUrl(p))
-  const d4 = rc('audio-lab.openServerUrl', openServerUrl)
-  const d5 = rc('audio-lab.openSettings', openSettings)
-  const d6 = rc('audio-lab.refreshServerStatus', () => p.refreshStatus())
-  const d7 = rc('audio-lab.revealInExplorer', revealInExplorer)
+  const d4 = rc('audio-lab.transcribeAudioFile', () => transcribeAudio(p))
+  const d5 = rc('audio-lab.changeServerUrl', () => changeServerUrl(p))
+  const d6 = rc('audio-lab.openServerUrl', openServerUrl)
+  const d7 = rc('audio-lab.openSettings', openSettings)
+  const d8 = rc('audio-lab.refreshServerStatus', () => p.refreshStatus())
 
-  context.subscriptions.push(d1a, d1b, d2, d3, d4, d5, d6, d7)
+  context.subscriptions.push(d1, d2, d3, d4, d5, d6, d7, d8)
 }
 
 export function deactivate() {
